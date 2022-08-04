2726623214ac072e33336e991c858522d2f28398
user <user@userui-MacBookAir.local>
2022-08-04 11:16:01 +0900

    Monolitic servic VPC 최종 생성

--- gateway.tf
+++ gateway.tf
@@ -2,3 +2,23 @@ resource "aws_internet_gateway" "monolitic-ig-tf" {
   vpc_id = aws_vpc.monolitic-vpc-tf.id
   tags = {"Name" = "Terraform resource"}
 }
+
+resource "aws_eip" "nat-eip-tf" {
+  vpc = true
+  lifecycle {
+    # 재생성 시 다운타임이 없도록 먼저 생성한 후 지우도록 하는 옵션
+    create_before_destroy = true
+  }
+
+  tags = {"Name" = "Terraform resource"}
+}
+
+resource "aws_nat_gateway" "monolitic-nat-gateway-tf" {
+  allocation_id = aws_eip.nat-eip-tf.id
+  subnet_id = aws_subnet.monolitic-public-subnet-tf.id
+
+  tags = {"Name" = "Terraform resource"}
+  depends_on = [
+    aws_eip.nat-eip-tf
+  ]
+}
---
+++ route_table.tf
@@ -0,0 +1,56 @@
+resource "aws_route_table" "monolitic-public-rt-tf" {
+  vpc_id = aws_vpc.monolitic-vpc-tf.id
+
+  tags = {"Name" = "Terraform resource"}
+}
+
+resource "aws_route_table_association" "mono-public-rt-asso-tf" {
+  subnet_id = aws_subnet.monolitic-public-subnet-tf.id
+  route_table_id = aws_route_table.monolitic-public-rt-tf.id
+  
+  depends_on = [
+    aws_route_table.monolitic-public-rt-tf, aws_subnet.monolitic-public-subnet-tf
+  ]
+}
+
+resource "aws_route_table" "monolitic-private1-rt-tf" {
+  vpc_id = aws_vpc.monolitic-vpc-tf.id
+  
+  tags = {"Name" = "Terraform resource"}
+}
+
+resource "aws_route_table" "monolitic-private2-rt-tf" {
+  vpc_id = aws_vpc.monolitic-vpc-tf.id
+  
+  tags = {"Name" = "Terraform resource"}
+}
+
+resource "aws_route_table_association" "mono-private1-rt-asso-tf" {
+  subnet_id = aws_subnet.monolitic-private-subnet1-tf.id
+  route_table_id = aws_route_table.monolitic-private1-rt-tf.id
+
+  depends_on = [
+    aws_subnet.monolitic-private-subnet1-tf, aws_route_table.monolitic-private1-rt-tf
+  ]
+}
+
+resource "aws_route_table_association" "mono-private2-rt-asso-tf" {
+  subnet_id = aws_subnet.monolitic-private-subnet2-tf.id
+  route_table_id = aws_route_table.monolitic-private2-rt-tf.id
+
+  depends_on = [
+    aws_subnet.monolitic-private-subnet2-tf, aws_route_table.monolitic-private2-rt-tf
+  ]
+}
+
+resource "aws_route" "mono-private1-route-tf" {
+  route_table_id = aws_route_table.monolitic-private1-rt-tf.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.monolitic-nat-gateway-tf.id
+}
+
+resource "aws_route" "mono-private2-route-tf" {
+  route_table_id = aws_route_table.monolitic-private2-rt-tf.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.monolitic-nat-gateway-tf.id
+}
