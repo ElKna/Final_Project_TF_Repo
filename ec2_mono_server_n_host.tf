b10efc1f552e182b644d0b4670aba3cb66df2a23
ElKna <yyt012@gmail.com>
2022-08-08 17:58:33 +0900

    Fix: Script for using user-data and fix error

--- ec2_mono_server_n_host.tf
+++ ec2_mono_server_n_host.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "bastion-host-tf" {
 }
 
 resource "aws_instance" "monolitic-server-tf" {
-  ami = "ami-0f9e1c368c45414d0"
+  ami = "ami-00143fc0a488dccad"
   instance_type = "t2.micro"
   key_name = "serverInstance"
   subnet_id = aws_subnet.monolitic-private-subnet1-tf.id
@@ -29,10 +29,11 @@ resource "aws_instance" "monolitic-server-tf" {
     aws_route_table_association.mono-private2-rt-asso-tf, 
     aws_route_table_association.mono-private2-rt-asso-tf, 
     aws_route.mono-private1-route-tf,
-    aws_route.mono-private2-route-tf
+    aws_route.mono-private2-route-tf,
+    aws_db_instance.mono-auth-db-tf
   ]
 
-  user_data = "${file("mono_server_init.sh")}"
+  user_data = "${file("mono_server_init.sh")} \n export MYSQL_HOSTNAME=${aws_db_instance.mono-auth-db-tf.address} MYSQL_PASSWORD=${aws_db_instance.mono-auth-db-tf.password} MYSQL_USERNAME=${aws_db_instance.mono-auth-db-tf.username} MYSQL_DATABASE=${aws_db_instance.mono-auth-db-tf.db_name} JWT_SECRET=devops \nmysql -h $MYSQL_HOSTNAME -u $MYSQL_USERNAME --password=$MYSQL_PASSWORD $MYSQL_DATABASE < ./aacompany_dump.sql \npm2 start 'npm run start' >> record.txt \n--//--"
 
   tags = {"Name" = "monolitic-server-tf"}
 }
--- mono_server_init.sh
+++ mono_server_init.sh
@@ -1,14 +1,37 @@
+Content-Type: multipart/mixed; boundary="//"
+MIME-Version: 1.0
+
+--//
+Content-Type: text/cloud-config; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="cloud-config.txt"
+
+#cloud-config
+cloud_final_modules:
+- [scripts-user, always]
+
+--//
+Content-Type: text/x-shellscript; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="userdata.txt"
+
 #!/bin/bash
-sudo apt update
-sudo apt install -y mysql-client
+cd /home/ubuntu
+sudo apt update >> record.txt
+sudo apt install -y mysql-client >> record.txt
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
-nvm install --lts
+nvm install --lts >> record.txt
 
-npm install pm2 -g
+npm install pm2 -g >> record.txt
 
-sudo apt install libcap2-bin 
+sudo apt install libcap2-bin
 sudo setcap cap_net_bind_service=+ep `readlink -f \`which node\``
+
+cd Final_project_test_source
+npm install >> record.txt
